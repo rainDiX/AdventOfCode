27518fec5dc69dcb4e6630d7d23dcc022c6b45ba
Romain Dijoux <raindx@irised.one>
2023-12-04 15:54:27 +0100

    cleaning

--- Day2/day2.cpp
+++ Day2/day2.cpp
@@ -5,19 +5,11 @@
 #include <string>
 #include <vector>
 
+#include "commonFn.hpp"
+
 #define RED 12
 #define GREEN 13
 #define BLUE 14
-
-void strSplit(const std::string_view str, const std::string_view delimiter,
-              std::vector<std::string_view>& vec) {
-  size_t begin = 0, end = 0;
-  while ((end = str.find(delimiter, begin)) != std::string::npos) {
-    vec.push_back(str.substr(begin, end - begin));
-    begin = end + delimiter.size();
-  }
-  vec.push_back(str.substr(begin));
-}
 
 class Game {
  public:
